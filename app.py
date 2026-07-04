4377bee347e0ae766fadb8908cfdc2dab7843b29
Shrikant Borude <shrikantborude31@gmail.com>
2026-07-04 17:36:07 +0530

    Add login and signup page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from cs50 import SQL
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False # Cookies should dissapear when the user quits the browser
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -18,6 +23,16 @@ def flicking():
 @app.route("/precision")
 def precision():
     return render_template("precision.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+
 
 # mainly for testing purposes if its (app.py as import app) imported then it does not run
 if __name__ == "__main__":
